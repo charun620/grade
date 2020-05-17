cc9928c8936305e940c07b05d48f1e57f54e5d30
charun620 <51948655+charun620@users.noreply.github.com>
2020-05-17 16:03:57 +0700

    Update CGrade.cpp

--- CGrade.cpp
+++ CGrade.cpp
@@ -7,20 +7,23 @@ int main()
  
 	cout << "Enter Number :";
 	cin >> n;
-	if ((n > 0)&& (n < 59))cout << "you got F";
-	else if (n > 60 && n < 69)
+	if ((n >= 0)&& (n <= 59))
+	{
+		cout << "you got F";
+	}
+	else if (n >= 60 && n <= 69)
 	{
 		cout << "you got D\nyou survived to F" << endl;
 	} 
-	else if (n > 70 && n < 79)
+	else if (n >= 70 && n <= 79)
 	{
 		cout << "you got C\nTry a little  bit" << endl;
 	}
-	else if (n > 80 && n < 89)
+	else if (n >= 80 && n <= 89)
 	{
 		cout << "you got B\ngoodwork" << endl;
 	}
-	else if (n > 90 && n < 100)
+	else if (n >= 90 && n <= 100)
 	{
 		cout << "you got A\ncongratulations" << endl;
 	}
